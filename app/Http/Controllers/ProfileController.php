37ab1ed54db6ef55b06757914082b333cd0e4494
Md. Naimul Islam <113783674+PicaPrime@users.noreply.github.com>
2025-01-25 22:54:29 +0600

    all 8 test passed in local machine

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\ProfileUpdateRequest;
 
 class ProfileController extends Controller
@@ -15,7 +15,15 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
-
+        $attributes = $request->validated();
+        $user = Auth::user();
+        $user->name = $attributes['name'];
+        $user->email = $attributes['email'];
+        if(isset($attributes['password'])) {
+            $user->password = bcrypt($attributes['password']);
+        }
+        /** @var \App\Models\User $user **/
+        $user->save();
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
